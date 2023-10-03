525fa7598cffd8c227b7ef7e97047312146ac4ad
MattyMatt357 <mc21517@my.acm.ac.uk>
2023-10-03 16:48:30 +0100

    PanelColourChanges
    Panel can change colour, but can sometimes stay as the previous colour

---
+++ Assets/Scripts/PanelChanges.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelChanges : MonoBehaviour
+{
+    public GameObject Panel;
+
+   
+
+    [SerializeField]
+    private Material PanelMaterial;
+
+    
+
+    public Color[] PanelColours;
+
+    public bool CanChangeColour;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        CanChangeColour = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangePanelColour();
+
+    }
+
+    public void ChangePanelColour()
+    {
+        if (CanChangeColour == true)
+        {                         
+            PanelMaterial.color = PanelColours[Random.Range(0, PanelColours.Length)];               
+            StartCoroutine("ColourTimer");
+        }
+    }
+
+    IEnumerator ColourTimer()
+    {
+        CanChangeColour = false;
+        yield return new WaitForSeconds(5f);
+        CanChangeColour = true;
+    }
+}
