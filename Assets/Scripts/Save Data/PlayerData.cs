cf33a0e47c608ae1170e243b874f002508290a42
Lewis <135326467+Paite-ACM@users.noreply.github.com>
2023-12-01 14:31:06 +0000

    Upgrade save data + additional targets in map

--- Assets/Scripts/Save Data/PlayerData.cs
+++ Assets/Scripts/Save Data/PlayerData.cs
@@ -1,14 +1,8 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 [System.Serializable]
 public class PlayerData
 {
     public float score;
     public float highScore;
-
     // add powerup stuff (and whatever else we're adding) later
 
     public PlayerData(GameManager gm)
--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -33,6 +33,18 @@ public class Upgrades : MonoBehaviour
         playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
         discThrow = GameObject.Find("Player").GetComponent<DiscThrow>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        // upgrade save data stuff
+        if (SaveSystem.DoesUpgradeFileExist())
+        {
+            LoadUpgradeData();
+            UpgradeCorrection();
+        }
+        else
+        {
+            SaveSystem.CreateUpgradeFile(this);
+            SaveData();
+        }
     }
 
     // Start is called before the first frame update
@@ -127,5 +139,46 @@ public class Upgrades : MonoBehaviour
         JumpHeightText.text = "Jump Height:" + playerMovement.jumpForce.ToString("F2");
         BallSpeedText.text = "Ball Speed:" + discThrow.speed.ToString("F2");
         PointGainText.text = "Base Score Gain:" + gameManager.baseScoreGive.ToString("F2");
+    }
+
+    public void LoadUpgradeData()
+    {
+        UpgradeData data = SaveSystem.LoadUpgrades();
+
+        upgradePlayerSpeedLimit = data.upgradePlayerSpeedLimit;
+        upgradeBallSpeedLimit = data.upgradeBallSpeedLimit;
+        upgradePointGainLimit = data.upgradePointGainLimit;
+        upgradePlayerJumpHeightLimit = data.upgradePlayerJumpHeightLimit;
+
+    }
+
+    public void SaveData()
+    {
+        SaveSystem.SaveUpgrades(this);
+    }
+
+    // called on start upon loading data in order to correct upgraded values
+    private void UpgradeCorrection()
+    {
+        // whole lotta for loops
+        for (int i = 0; i < upgradePlayerSpeedLimit; i++)
+        {
+            playerMovement.movementSpeed *= 1.05f;
+        }
+
+        for (int i = 0; i < upgradeBallSpeedLimit; i++)
+        {
+            discThrow.speed *= 1.05f;
+        }
+
+        for (int i = 0; i < upgradePointGainLimit; i++)
+        {
+            gameManager.baseScoreGive *= 1.05f;
+        }
+
+        for (int i = 0; i < upgradePlayerJumpHeightLimit; i++)
+        {
+            playerMovement.jumpForce *= 1.03f;
+        }
     }
 }
